af029bd1a3a660431b92e316e1bddae8c23258c8
Alex Bondar <abondar1992@gmail.com>
2026-07-08 12:48:21 +0200

    add user input for yellow led blinks

--- yellow_blue_v2/yellow_blue_v2.ino
+++ yellow_blue_v2/yellow_blue_v2.ino
@@ -1,7 +1,7 @@
 int yellowLedPin = 9;
 int blueLedPin = 2;
 
-int yellowBlink = 5;
+int yellowBlink;
 int blueBlink = 3;
 
 int yellowDelayOnMs = 100;
@@ -21,6 +21,16 @@ void setup() {
 
   pinMode(yellowLedPin, OUTPUT);
   pinMode(blueLedPin, OUTPUT);  
+
+  Serial.println("Enter number of yellow blinks");
+  while(Serial.available()==0){
+    
+  }
+
+  int input = Serial.parseInt();
+  yellowBlink = input == 0 ? 5: input;
+  Serial.read();
+
 }
 
 void loop() {
