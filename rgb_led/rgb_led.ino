a650569a4409761b7886e1b35c99dd890f228fb6
Alex Bondar <abondar1992@gmail.com>
2026-07-10 14:52:48 +0200

    add input for rgb led

--- rgb_led/rgb_led.ino
+++ rgb_led/rgb_led.ino
@@ -6,8 +6,9 @@ int redBrightness = 50;
 int blueBrightness = 20;
 int greenBrightness = 10;
 
+String color;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
@@ -17,8 +18,39 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(redPin, redBrightness);
-  analogWrite(bluePin, blueBrightness);
-  analogWrite(greenPin, greenBrightness);
+  
+  Serial.println("Enter a desired color ");
+  while(Serial.available()==0){
+
+  }
+
+  color = Serial.readString();
+
+  while (Serial.available() > 0) {
+    Serial.read();
+  }
+
+  if (color == "red"){
+     analogWrite(redPin, redBrightness);
+     analogWrite(bluePin, 0);
+     analogWrite(greenPin, 0);
+  }
+
+  if (color == "blue"){
+     analogWrite(bluePin, blueBrightness);
+     analogWrite(redPin, 0);
+     analogWrite(greenPin, 0);
+  }
+  
+  if (color == "green"){
+    analogWrite(greenPin, greenBrightness);
+    analogWrite(redPin, 0);
+    analogWrite(bluePin, 0);
+  }
+
+  if (color == "mix"){
+    analogWrite(redPin, redBrightness);
+    analogWrite(bluePin, blueBrightness);
+    analogWrite(greenPin, greenBrightness);
+  }
 }
